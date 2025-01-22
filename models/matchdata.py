f5f96091a4203d4262d7993dbb1725840f24e02b
Anthony Galea <anthonycgalea@gmail.com>
2025-01-22 18:24:28 -0500

    Superscout and match data

--- models/matchdata.py
+++ models/matchdata.py
@@ -85,10 +85,81 @@ class MatchData(db.Model):
         self.matchNumber = data["matchNumber"]
         self.matchLevel = getCurrentMatchLevel()
         #insert gamespecific TODO
-        
+        self.al4ca = int(data["autoL4Coral"][0])
+        self.al4cb = int(data["autoL4Coral"][1])
+        self.al4cc = int(data["autoL4Coral"][2])
+        self.al4cd = int(data["autoL4Coral"][3])
+        self.al4ce = int(data["autoL4Coral"][4])
+        self.al4cf = int(data["autoL4Coral"][5])
+        self.al3ca = int(data["autoL3Coral"][0])
+        self.al3cb = int(data["autoL3Coral"][1])
+        self.al3cc = int(data["autoL3Coral"][2])
+        self.al3cd = int(data["autoL3Coral"][3])
+        self.al3ce = int(data["autoL3Coral"][4])
+        self.al3cf = int(data["autoL3Coral"][5])
+        self.al2ca = int(data["autoL2Coral"][0])
+        self.al2cb = int(data["autoL2Coral"][1])
+        self.al2cc = int(data["autoL2Coral"][2])
+        self.al2cd = int(data["autoL2Coral"][3])
+        self.al2ce = int(data["autoL2Coral"][4])
+        self.al2cf = int(data["autoL2Coral"][5])
+        self.al1ca = int(data["autoL1Coral"][0])
+        self.al1cb = int(data["autoL1Coral"][1])
+        self.al1cc = int(data["autoL1Coral"][2])
+        self.al1cd = int(data["autoL1Coral"][3])
+        self.al1ce = int(data["autoL1Coral"][4])
+        self.al1cf = int(data["autoL1Coral"][5])
+        self.tl4ca = int(data["teleL4Coral"][0])
+        self.tl4cb = int(data["teleL4Coral"][1])
+        self.tl4cc = int(data["teleL4Coral"][2])
+        self.tl4cd = int(data["teleL4Coral"][3])
+        self.tl4ce = int(data["teleL4Coral"][4])
+        self.tl4cf = int(data["teleL4Coral"][5])
+        self.tl3ca = int(data["teleL3Coral"][0])
+        self.tl3cb = int(data["teleL3Coral"][1])
+        self.tl3cc = int(data["teleL3Coral"][2])
+        self.tl3cd = int(data["teleL3Coral"][3])
+        self.tl3ce = int(data["teleL3Coral"][4])
+        self.tl3cf = int(data["teleL3Coral"][5])
+        self.tl2ca = int(data["teleL2Coral"][0])
+        self.tl2cb = int(data["teleL2Coral"][1])
+        self.tl2cc = int(data["teleL2Coral"][2])
+        self.tl2cd = int(data["teleL2Coral"][3])
+        self.tl2ce = int(data["teleL2Coral"][4])
+        self.tl2cf = int(data["teleL2Coral"][5])
+        self.tl1ca = int(data["teleL1Coral"][0])
+        self.tl1cb = int(data["teleL1Coral"][1])
+        self.tl1cc = int(data["teleL1Coral"][2])
+        self.tl1cd = int(data["teleL1Coral"][3])
+        self.tl1ce = int(data["teleL1Coral"][4])
+        self.tl1cf = int(data["teleL1Coral"][5])
+        self.al3aa = int(data["autoL3Algae"][0])
+        self.al3ab = int(data["autoL3Algae"][1])
+        self.al3ac = int(data["autoL3Algae"][2])
+        self.al2aa = int(data["autoL2Algae"][0])
+        self.al2ab = int(data["autoL2Algae"][1])
+        self.al2ac = int(data["autoL2Algae"][2])
+        self.tl3aa = int(data["teleL3Algae"][0])
+        self.tl3ab = int(data["teleL3Algae"][1])
+        self.tl3ac = int(data["teleL3Algae"][2])
+        self.tl2aa = int(data["teleL2Algae"][0])
+        self.tl2ab = int(data["teleL2Algae"][1])
+        self.tl2ac = int(data["teleL2Algae"][2])
+        self.aN = int(data["autoNet"])
+        self.aP = int(data["autoProcessor"])
+        self.tN = int(data["teleNet"])
+        self.tP = int(data["teleProcessor"])
+        if data["endgame"] == 1:
+            self.endgameShallow = 1
+        else:
+            self.endgameShallow = 0
+        if data["endgame"] == 2:
+            self.endgameDeep = 1
+        else: 
+            self.endgameDeep = 0
         self.tablet = data["tablet"]
         self.scouter = data["scouter"]
-        self.timestamp = datetime.datetime.now()
+        self.timestamp = datetime.now()
 
     def __str__(self):
         return self.teamNumber
